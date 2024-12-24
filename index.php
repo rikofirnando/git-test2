0fddd93b747d8db0dc70ac6edbde07bc9757e2a4
rikofirnando <rikofirnando73@yahoo.com>
2024-12-24 12:07:45 +0700

    Menambahkan paragraf, h1, dan title

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Try Git Remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>saya sedang mencoba Git Remote</p>
+</body>
+</html>
